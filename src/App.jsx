c2e532978f6c6247ed064ef77e754b555450eaee
Vadim Kim <vkim0203@gmail.com>
2024-12-09 17:30:56 +0900

    player component and buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import React from "react";
+import Player from "./components/Player.jsx";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Yuliya" symbol="X" />
+          <Player name="Vadim" symbol="O" />
+        </ol>
+        {/* Game Board */}
+      </div>
+
+      {/* LOG */}
+    </main>
+  );
 }
 
-export default App
+export default App;
